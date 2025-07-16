7c209156abff4ebf038093112371dd4051f48d3c
naila-ghulamrasool <nailaghulam99@gmail.com>
2025-07-16 09:12:19 +0500

    Add files via upload

---
+++ Ass2.js
@@ -0,0 +1,22 @@
+let username;
+let myName = "john Doe";
+let message;             // a) Declare variable
+message = "Hello World"; // b) Assign variable
+alert(message);          // c) Display in alert box
+let studentName = "john Doe";
+let studentAge = "18 years old";
+let studentCourse = "Web and App Development";
+alert(studentName);
+alert(studentAge);
+alert(studentCourse);
+let food;
+food="PIZZA\n PIZZ\n PIZ\n PI\n P";
+alert(food);
+let email;
+email="example@example.com";
+alert("My email address is "+ email);
+let book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the Book " + book);
+document.writeln("Yah! I can write HTML content through JavaScript");
+alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+
